0a78277c75d569f52903ce390f1d1569379ebb7c
Eric Newton <eric.newton@lightmaker.com>
2014-12-18 08:57:51 -0500

    added error branch in the build script

--- build.cmd
+++ build.cmd
@@ -26,10 +26,12 @@ msbuild %SOURCEPATH%\CPCodeSyncronize\CPCodeSyncronize.csproj /p:Configuration=R
 ECHO.
 ECHO Performing merge:
 mkdir %BUILDPATH%\NugetPack 2>NUL
+if errorlevel 1 goto :ERROR
 
 pushd %BUILDPATH%\bin\%config%
 ECHO ILMerge /out:%BUILDPATH%\Nugetpack\cpcodesync.exe cpcodesync.exe CommandLine.dll
 %PACKAGES%\ILMerge.2.13.0307\ilmerge.exe /out:%BUILDPATH%\Nugetpack\cpcodesync.exe cpcodesync.exe CommandLine.dll
+if errorlevel 1 goto :ERROR
 ECHO Done.
 popd
 
@@ -38,7 +40,9 @@ ECHO.
 ECHO Building Nuget Package:
 pushd %BUILDPATH%\NugetPack
 copy %~dp0\CPCodeSync.CommandLine.nuspec .
+if errorlevel 1 goto :ERROR
 %PACKAGES%\Nuget.CommandLine.2.8.2\tools\Nuget.exe pack CPCodeSync.CommandLine.nuspec -Version %BUILD_VERSION%
+if errorlevel 1 goto :ERROR
 popd
 
 :NUGET_UPLOAD
@@ -46,7 +50,15 @@ ECHO.
 ECHO Uploading Nuget package:
 pushd %BUILDPATH%\NugetPack
 %PACKAGES%\Nuget.CommandLine.2.8.2.\tools\Nuget.exe push CPCodeSync.CommandLine.%BUILD_VERSION%.nupkg -Source http://nuget.lightmaker.com/
+if errorlevel 1 goto :ERROR
 popd
 
+goto :END
+
+:ERROR
+popd
+echo ERROR occurred.
+
+:END
 ECHO.
 tree %BUILDPATH% /F
