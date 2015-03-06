68a1270f7ea99cdfc30d949b4fcf8fe285858c35
Eric Newton <eric.newton@lightmaker.com>
2015-03-06 12:51:55 -0500

    refactoring: moved Extract command code into separate class

---
+++ Source/CPCodeSyncronize/ExtractCommand.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApplication1
+{
+	public class ExtractCommand
+	{
+
+		public ExtractOptions Options;
+
+		public void PreExecute()
+		{
+
+		}
+
+		public void Execute()
+		{
+			string filename;
+
+			string fullUri = Options.InputFile;// "http://dev-retailnationalgrid.nationalgridaccess.com/codelibrary.xml";
+			if (Options.OutputDir != null && Directory.Exists(Options.OutputDir) == false)
+			{
+				if (Options.CreateDir != false)
+				{
+					if (Options.CreateDir == null)
+					{
+						Console.Write("basepath '{0}' doesn't exist, would you like to create it? (y/n)", Options.OutputDir);
+						var key = Console.ReadKey();
+						Console.WriteLine();
+						if (key.Key == ConsoleKey.Y) Options.CreateDir = true;
+					}
+
+					if (Options.CreateDir == true)
+					{
+						if (Options.Verbose) Console.WriteLine("Creating directory '{0}'.", Options.OutputDir);
+						if (Options.DryRun == false) Directory.CreateDirectory(Options.OutputDir);
+					}
+				}
+			}
+
+			if (fullUri.StartsWith("http://") || fullUri.StartsWith("https://"))
+			{
+				filename = fullUri.Substring(fullUri.LastIndexOf("/") + 1);
+				if (File.Exists(filename) == false)
+				{
+
+					System.Net.WebClient wc = new System.Net.WebClient();
+					wc.DownloadFile(fullUri, filename);
+				}
+			}
+			else
+			{
+				filename = Options.InputFile;
+			}
+
+			if (Options.OutputDir == null)
+			{
+				Options.OutputDir = Path.GetDirectoryName(filename);
+				if (Options.OutputDir == "")
+				{
+					Options.OutputDir = ".\\";
+				}
+			}
+
+			if (Options.Verbose)
+			{
+				Console.WriteLine("OutputDir: {0}", Options.OutputDir);
+			}
+			WriteFiles(filename);
+		}
+
+
+		static IEnumerable<XElement> LoadFromFile(string filename)
+		{
+			Stream s = null;
+			try
+			{
+				s = File.OpenRead(filename);
+
+				if (filename.EndsWith(".gz"))
+				{
+					s = new System.IO.Compression.GZipStream(s, System.IO.Compression.CompressionMode.Decompress);
+				}
+
+				return StreamCodeFileElements(s, "codeFile");
+			}
+			finally
+			{
+
+			}
+		}
+
+		static IEnumerable<XElement> StreamCodeFileElements(Stream source, string elementName)
+		{
+			using (XmlReader reader = XmlReader.Create(source))
+			{
+				XElement item = null;
+
+				reader.MoveToContent();
+
+				// Parse the file, save header information when encountered, and yield the
+				// Item XElement objects as they are created.
+
+				// loop through codeFile elements
+				while (reader.Read())
+				{
+					if (reader.NodeType == XmlNodeType.Element
+						&& reader.Name == elementName)
+					{
+						item = XElement.ReadFrom(reader) as XElement;
+
+						if (item != null) yield return item;
+					}
+				}
+			}
+		}
+
+		static Stream PrepareInputStream(byte[] contentBytes)
+		{
+			Stream inputStream = new MemoryStream(contentBytes);
+
+			if (contentBytes[0] == 0x1f && contentBytes[1] == 0x8b)
+			{
+				var gzip = new System.IO.Compression.GZipStream(inputStream, System.IO.Compression.CompressionMode.Decompress);
+
+				return gzip;
+			}
+
+			return inputStream;
+		}
+
+		static void ScanElementsPath(string filename, ExtractOptions options)
+		{
+			IEnumerable<XElement> codeFileElements = LoadFromFile(filename);
+
+			List<string> relPaths = new List<string>(100);
+
+			foreach (var filenode in codeFileElements)
+			{
+				relPaths.Add(filenode.Attribute("name").Value);
+			}
+
+		}
+
+
+		void WriteFiles(string filename)
+		{
+			IEnumerable<XElement> codeFileElements = LoadFromFile(filename);
+
+			foreach (var filenode in codeFileElements)
+			{
+				WriteFile(filenode, Options.OutputDir);
+			}
+		}
+
+
+		void WriteFile(XElement node, string basepath)
+		{
+			try
+			{
+				string name = node.Attribute("name").Value;
+
+				string filepath;
+
+				if (name.StartsWith("/"))
+					filepath = name.Substring(1).Replace("/", "\\");
+				else
+					filepath = name.Replace("/", "\\");
+
+				EnsureDirectories(filepath, basepath);
+
+				string fullpath = Path.Combine(basepath, filepath);
+
+				if (node.Value != "")
+				{
+					byte[] base64contentgzip = Convert.FromBase64String(node.Value.Trim());
+
+					Stream outputStream = null;
+					try
+					{
+						if (Options.DryRun == false)
+							outputStream = new FileStream(fullpath, FileMode.Create, FileAccess.Write);
+						else
+							outputStream = new MemoryStream();
+
+						using (Stream inputStream = PrepareInputStream(base64contentgzip))
+						{
+							int bytesRead = -1;
+							do
+							{
+								bytesRead = inputStream.Read(base64contentgzip, 0, base64contentgzip.Length);
+								if (Options.DryRun == false) outputStream.Write(base64contentgzip, 0, bytesRead);
+							} while (bytesRead > 0);
+						}
+
+						outputStream.Flush();
+					}
+					finally
+					{
+						if (outputStream != null) outputStream.Dispose();
+					}
+				}
+				else
+				{
+					//Log.Warn("file node '{0}' contained no content.", node.Attribute("name").Value);
+				}
+
+				if (Options.Verbose)
+					Console.WriteLine("Wrote file '{0}'.", fullpath);
+				else
+					Console.Write(".");
+			}
+			catch (Exception ex)
+			{
+				if (Options.Verbose == false) Console.WriteLine();
+				string nameAttrValue = ""; try { nameAttrValue = node.Attribute("name").Value; }
+				catch { }
+				Console.Error.WriteLine("Failed on node for file '{0}'.\n{1}", nameAttrValue, ex.ToString());
+				Console.Error.WriteLine(node.ToString());
+			}
+
+		}
+
+		void EnsureDirectories(string filepath, string basepath)
+		{
+			if (Options.DryRun) return;
+			if (string.IsNullOrEmpty(basepath)) basepath = ".\\";
+			if (Directory.Exists(basepath) == false) throw new InvalidOperationException("basepath doesn't exist");
+
+			string[] segments = filepath.Split('\\');
+
+			int index = 0;
+			int segmentsLengthMinusOne = segments.Length - 1;
+
+			while (index < segmentsLengthMinusOne)
+			{
+				string directorypath = Path.Combine(basepath, String.Join("\\", segments.Take(index + 1)));
+				index++;
+
+				try
+				{
+					if (Directory.Exists(directorypath) == false)
+					{
+						Directory.CreateDirectory(directorypath);
+					}
+				}
+				catch { throw; }
+			}
+		}
+	}
+}
