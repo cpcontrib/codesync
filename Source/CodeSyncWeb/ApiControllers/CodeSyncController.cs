88f48aadac56e74bac7deb1ef46387750492838e
Eric Newton <ericnewton76@gmail.com>
2017-12-11 19:35:54 -0500

    small changes to log info

--- Source/CodeSyncWeb/ApiControllers/CodeSyncController.cs
+++ Source/CodeSyncWeb/ApiControllers/CodeSyncController.cs
@@ -12,36 +12,50 @@ using ConfigurationManager = System.Configuration.ConfigurationManager;
 
 namespace CodeSyncWeb.ApiControllers
 {
-	[RoutePrefix("v1")]
+	[RoutePrefix("api/v1")]
+
+
 	public class UploadController : ApiController
 	{
 		private static NLog.ILogger Log = NLog.LogManager.GetCurrentClassLogger();
 
-		private static string S_UploadFolder = System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/Uploads");
+		private static string S_UploadPath = System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/Uploads");
+
+		//constructor injection
+		//public UploadController(NLog.ILogger Log, IFileSystem FileSystem, string UploadPath)
+
 
 		//[ValidateMimeMultipartContentFilter]
 		[Route("upload/{clientLibrary}")]
 		public async Task<IHttpActionResult> PostUpload(string clientLibrary)
 		{
+			Log.Info("Receiving upload for clientLibrary='{0}'.", clientLibrary);
+
+			Log.Debug("acquiring stream");
 			var x = await Request.Content.ReadAsStreamAsync();
 
-			string targetPath = Path.Combine(S_UploadFolder, clientLibrary + ".xml");
+			string targetRel= "~/App_Data/Uploads" + "/" + clientLibrary + ".xml";
 
 			IEnumerable<string> values;
 			if(Request.Headers.TryGetValues("Content-Type", out values))
 			{
 				if(values.FirstOrDefault() == "application/x-gzip") 
-					targetPath += ".gz";
+					targetRel += ".gz";
 			}
+
+			Log.Debug("targetRel='{0}'.", targetRel);
+
+			string targetPath = Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/Uploads"), targetRel);
 
 			using(FileStream fs = new FileStream(targetPath, FileMode.Create, FileAccess.Write, FileShare.Read))
 			{
-				Log.Info("writing to '{0}'.", targetPath);
+				Log.Debug("writing to '{0}'.", targetPath);
 				await x.CopyToAsync(fs);
 			}
 
 			new Task(() =>
 				{
+					Log.Info("Sending notification email.");
 					SendNotificationEmail(clientLibrary);
 				}).Start();
 
@@ -54,7 +68,7 @@ namespace CodeSyncWeb.ApiControllers
 		public async Task<IHttpActionResult> PostContent()
 		{
 
-			var streamProvider = new MultipartFormDataStreamProvider(S_UploadFolder);
+			var streamProvider = new MultipartFormDataStreamProvider("");
 			await Request.Content.ReadAsMultipartAsync(streamProvider);
 
 			return StatusCode(HttpStatusCode.Accepted);
@@ -96,7 +110,7 @@ namespace CodeSyncWeb.ApiControllers
 			catch(Exception ex)
 			{
 				Log.Error(ex, "Failed to send email.");
-				Log.Warn("Dumping mail message to log.\n{0}", msg.Body.ToString());
+				Log.Warn("Dumping mail message to log:\n----------------------\n{0}----------------------\n", msg.Body.ToString());
 				throw;
 			}
 		}
