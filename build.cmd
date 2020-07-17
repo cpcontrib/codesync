c723c9cbdaaebd518337c451d00b9ee27bb8fe7f
Eric Newton <ericnewton76@gmail.com>
2020-07-17 02:36:40 -0400

    perfected the build

--- build.cmd
+++ build.cmd
@@ -9,9 +9,13 @@ set PACKAGES=%~dp0\packages
 
 REM get buildversion from build environment, if available.
 if not "%APPVEYOR_BUILD_VERSION%" == "" set BUILD_VERSION=%APPVEYOR_BUILD_VERSION%
+if not "%1" == "" set BUILD_VERSION=%1
 if "%BUILD_VERSION%" == "" echo ERROR: BUILD_VERSION not set && goto :END
 
 if not exist "%BUILDPATH%\." goto :END
+
+set ILMERGE_EXE=%PACKAGES%\ILMerge.3.0.29\tools\net452\ILMerge.exe
+set NUGET_EXE=%PACKAGES%\Nuget.CommandLine.5.6.0\tools\Nuget.exe
 
 :CLEAN
 ECHO.
@@ -22,7 +26,7 @@ for /d %%d in (*) do rmdir /s /q %%d
 echo %BUILD_VERSION% >%BUILDPATH%\LastBuildVersion
 
 :BUILD
-msbuild %SOURCEPATH%\CPCodeSyncronize\CPCodeSyncronize.csproj /p:Configuration=Release;OutputPath=%BUILDPATH%\bin\%config%
+msbuild %SOURCEPATH%\codesync-cli\codesync-cli.csproj /p:Configuration=Release /p:OutputPath=%BUILDPATH%\Release
 if errorlevel 1 goto :ERROR
 
 :MERGE
@@ -31,12 +35,14 @@ ECHO Performing merge:
 mkdir %BUILDPATH%\NugetPack 2>NUL
 if errorlevel 1 goto :ERROR
 
-pushd %BUILDPATH%\bin\%config%
-ECHO ILMerge /out:%BUILDPATH%\Nugetpack\cpcodesync.exe cpcodesync.exe CommandLine.dll CPCodeSyncronizeCore.dll
-%PACKAGES%\ILMerge.2.13.0307\ilmerge.exe /out:%BUILDPATH%\Nugetpack\cpcodesync.exe cpcodesync.exe CommandLine.dll CPCodeSyncronizeCore.dll
+cd %BUILDPATH%\Release
 if errorlevel 1 goto :ERROR
-ECHO Done.
-popd
+
+echo.
+ECHO ILMerge /out:%BUILDPATH%\Nugetpack\codesync.exe codesync.exe CommandLine.dll codesync-core.dll
+%ILMERGE_EXE% /out:%BUILDPATH%\Nugetpack\codesync.exe codesync.exe CommandLine.dll codesync-core.dll
+if errorlevel 1 goto :ERROR
+ECHO ILMerge successful
 
 :NUGET_PACKAGE
 ECHO.
@@ -44,7 +50,8 @@ ECHO Building Nuget Package:
 pushd %BUILDPATH%\NugetPack
 copy %~dp0\CPCodeSync.CommandLine.nuspec .
 if errorlevel 1 goto :ERROR
-%PACKAGES%\Nuget.CommandLine.2.8.6\tools\Nuget.exe pack CPCodeSync.CommandLine.nuspec -Version %BUILD_VERSION%
+
+%NUGET_EXE% pack CPCodeSync.CommandLine.nuspec -Version %BUILD_VERSION%
 if errorlevel 1 goto :ERROR
 popd
 
@@ -52,7 +59,7 @@ popd
 ECHO.
 ECHO Uploading Nuget package:
 pushd %BUILDPATH%\NugetPack
-%PACKAGES%\Nuget.CommandLine.2.8.6\tools\Nuget.exe push CPCodeSync.CommandLine.%BUILD_VERSION%.nupkg -verbosity detailed
+REM %NUGET_EXE% push CPCodeSync.CommandLine.%BUILD_VERSION%.nupkg -verbosity detailed
 if errorlevel 1 goto :ERROR
 popd
 
@@ -64,4 +71,4 @@ echo ERROR occurred.
 
 :END
 ECHO.
-tree %BUILDPATH% /F
+tree %BUILDPATH% /f /a
